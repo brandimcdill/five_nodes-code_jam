a7803d7faec7186bce7cf081ea9ee71b320ffd7d
MrWatson0531 <135671943+MrWatson0531@users.noreply.github.com>
2026-02-15 14:12:18 -0700

    HTML refactor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import {Routes, Route} from "react-router-dom";
+import Header from "../Header/Header";
+import Cards from "../Cards/Cards";
+import People from "../People/People";
+import Footer from "../Footer/Footer";
 
 import CalendarComponent from "../Calendar/Calendar";
 
