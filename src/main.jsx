d7ba35379e4c5a2e035ed95ad65274e3a2f571e9
Brandi McDill <mcdillbrandig@gmail.com>
2026-02-15 13:28:26 -0500

    added app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './components/App/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename="/five_nodes-code_jam/">
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
