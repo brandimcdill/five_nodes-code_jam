605b4a85838aad69a286e45a36040ab221f56d0f
brandimcdill <mcdillbrandig@gmail.com>
2026-02-16 16:46:29 -0500

    fixed curly brace in app.jsx

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -5,8 +5,8 @@ function People() {
   return (
     <section className="your-people">
       <h2 className="your-people__title">Your Connections</h2>
-      <button type="button" class="your-people__add-btn">
-        <img src={add_icon} alt="" class="your-people__add-btn-image" />
+      <button type="button" className="your-people__add-btn">
+        <img src={add_icon} alt="" className="your-people__add-btn-image" />
       </button>
       <ul className="your-people__list">
         <Cards />
