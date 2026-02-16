76a1e03591017c4c9a3583d63e3a47ff2e952661
MrWatson0531 <135671943+MrWatson0531@users.noreply.github.com>
2026-02-16 12:59:36 -0700

    auth.js added

--- src/Utils/auth.js
+++ src/Utils/auth.js
@@ -1,3 +1,40 @@
+import {baseUrl} from "./api";
+import {checkResponse} from "./api";
+
 function getToken() {
   return localStorage.getItem("jwt");
 }
+
+function setToken(){
+    return localStorage.setItem("jwt");
+}
+
+function signUp(data){
+    return fetch(`${baseUrl}/signup`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data }),
+      }).then(checkResponse);
+}
+
+function signIn(data){
+    return fetch(`${baseUrl}/signin`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data }),
+      }).then(checkResponse);
+}
+
+function signOut(){
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("user");
+    return Promise.resolve();
+}
+
+export { getToken, setToken, signUp, signIn, signOut };
