5f6fa512f1e0be781b6e5a09f59447fcd493f3ab
brandimcdill <mcdillbrandig@gmail.com>
2026-02-17 14:15:42 -0500

    created error.js

--- src/Utils/error.js
+++ src/Utils/error.js
@@ -1,9 +1,12 @@
 const ERROR_TYPES = {
   BAD_REQUEST: { statusCode: 400, message: "Invalid data" },
-  UNAUTHORIZED: { statusCode: 401, message: "Unauthorized" },
-  FORBIDDEN: { statusCode: 403, message: "Forbidden" },
-  NOT_FOUND: { statusCode: 404, message: "Not found" },
-  INTERNAL_SERVER_ERROR: { statusCode: 500, message: "Internal server error" },
+  UNAUTHORIZED: { statusCode: 401, message: "Unauthorized access" },
+  FORBIDDEN: { statusCode: 403, message: "Access forbidden" },
+  NOT_FOUND: { statusCode: 404, message: "Resource not found" },
+  INTERNAL_SERVER_ERROR: {
+    statusCode: 500,
+    message: "An error has occured on the server",
+  },
 };
 
 module.exports = { ERROR_TYPES };
