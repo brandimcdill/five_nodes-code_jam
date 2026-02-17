87429d5c4f7cf983a2e2e1375973e57d01a9a135
Genevive Oliva <geneviveoliva95@gmail.com>
2026-02-16 20:11:06 -0800

    Add style to Create New Connection modal according to design specifications

--- src/components/Modals/CreatePersonModal.jsx
+++ src/components/Modals/CreatePersonModal.jsx
@@ -1,51 +1,67 @@
+import "./CreatePersonModal.css";
+
 function CreatePersonModal({ modal, setModal }) {
-    return (
-        <div class="modal" id="create-new-person">
-        <div class="modal__container">
-          <h2 class="modal__title">Create New Person</h2>
-          <form id="" action="" class="modal__form" novalidate>
-            <label for="person-name-input" class="modal__label"
-              >Person Name
-              <input
-                id="person-name-input"
-                type="text"
-                class="modal__input"
-                name="name"
-                placeholder="e.g., Juan"
-                minlength="2"
-                maxlength="40"
-                required
+  return (
+    <div
+      className={`modal ${modal === "createPerson" ? "modal_opened" : ""}`}
+      id="create-new-person"
+    >
+      <h2 className="modal__title">Create New Connection</h2>
+      <div className="modal__container">
+        <form id="" action="" className="modal__form" novalidate>
+          <label for="person-name-input" className="modal__label">
+            Person Name *
+            <input
+              id="person-name-input"
+              type="text"
+              className="modal__input"
+              name="name"
+              placeholder="e.g., Juan"
+              minlength="2"
+              maxlength="40"
+              required
+            />
+            <span className="modal__error"></span>
+          </label>
+          <label for="person-relationship-input" className="modal__label">
+            Relationship
+            <input
+              id="person-relationship-input"
+              type="text"
+              className="modal__input"
+              name="name"
+              placeholder="e.g., best friend"
+              minlength="2"
+              maxlength="40"
+            />
+          </label>
+          <label for="person-avatar" className="modal__label">
+            Avatar
+            <input
+              id="person-avatar"
+              type="url"
+              className="modal__input"
+              name="link"
+              placeholder=""
+            />
+          </label>
+          <div className="modal__btn-container">
+            <button className="modal__btn modal__btn-cancel">
+              <img
+                src=""
+                alt="Cross mark"
+                className="modal__cancel-btn-image"
               />
-              <span class="modal__error"></span>
-            </label>
-            <label for="person-nickname-input" class="modal__label">
-              Nickname (optional)
-              <input
-                id="person-nickname-input"
-                type="text"
-                class="modal__input"
-                name="name"
-                placeholder="e.g., best friend"
-                minlength="2"
-                maxlength="40"
-              />
-            </label>
-            <label for="person-avatar" class="modal__label">
-              Avatar (optional)
-              <input
-                id="person-avatar"
-                type="url"
-                class="modal__input"
-                name="link"
-                placeholder=""
-              />
-            </label>
-            <button class="modal__btn modal__btn-cancel">
-              <img src="" alt="Cross mark" class="modal__cancel-btn-image" />
               Cancel
             </button>
-            <button class="modal__btn modal__btn-create">Create</button>
-          </form>
-        </div>
-      </div>)}
-      export default CreatePersonModal;
+            <button className="modal__btn modal__btn-create">
+              <img src="" alt="Add sign" className="modal__btn-add" />
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default CreatePersonModal;
