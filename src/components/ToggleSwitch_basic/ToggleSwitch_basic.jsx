4627a5b00a7aec1ff90c255295dbf2884bc700f9
Brandi McDill <mcdillbrandig@gmail.com>
2026-02-18 14:39:10 -0500

    toggle switch adjustments

--- src/components/ToggleSwitch_basic/ToggleSwitch_basic.jsx
+++ src/components/ToggleSwitch_basic/ToggleSwitch_basic.jsx
@@ -2,9 +2,9 @@ import './ToggleSwitch_basic.css';
 
 function ToggleSwitch({  }) {
     return (
-        <label className="toggle-switch">
-            <input type="checkbox" id="toggle" className="toggle-switch__checkbox" />
-            <span className="toggle-switch__circle"></span>
+        <label className="toggle_switch">
+            <input type="checkbox" id="toggle" className="toggle_switch__checkbox" />
+            <span className="toggle_switch__circle"></span>
           
         </label>)}
 
