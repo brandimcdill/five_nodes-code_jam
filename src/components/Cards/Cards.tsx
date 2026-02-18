b5add55b5e8c8b567680ec0c2d32a00631959090
Genevive Oliva <geneviveoliva95@gmail.com>
2026-02-18 11:40:36 -0800

    Position cards list items with flexbox

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,6 +1,8 @@
 import "./Cards.css";
 import paper_plane from "../../assets/paper_plane.svg";
+import cards_ellipsis from "../../assets/cards_ellipsis.svg"
 import "./Cards.css";
+
 function Cards({ onclick, card }) {
   const handleCardClick = () => {
     onclick(card);
@@ -9,8 +11,8 @@ function Cards({ onclick, card }) {
   return (
     <li className="card">
       <div className="cards__information">
-        <button type="button" className="cards__button-pencil">
-          <img src="" alt="" className="cards__button-pencil-image" />
+        <button type="button" className="cards__button-ellipsis">
+          <img src={cards_ellipsis} alt="Circle ellipsis inside" className="cards__button-ellipsis-image" />
         </button>
         <img src={card.avatar} alt={card.name} className="avatar" />
         <h2 className="card__name">{card.name}</h2>
@@ -23,7 +25,7 @@ function Cards({ onclick, card }) {
           <img
             src={paper_plane}
             alt="Paper Plane Icon"
-            className="cards__button-calendar"
+            className="cards__button-calendar-image"
           />
           Tap to Open
         </button>
