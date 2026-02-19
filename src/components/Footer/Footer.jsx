410fc4ed9262d5471ace229ede83fa1774e2b651
brandimcdill <mcdillbrandig@gmail.com>
2026-02-19 11:48:14 -0500

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,23 @@
-import NavBar from "../NavBar/NavBar";
+import "./Footer.css";
+import footer_arrow from "../../assets/footer_arrow.svg";
 function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          <p className="footer-text">
+            <img src={footer_arrow} alt="Footer Arrow" /> © 2026 Paper Thought
+            Inc.
+          </p>
+        </div>
+        <div className="footer-right">
+          <p className="footer-link">Privacy</p>
+          <p className="footer-link">Terms</p>
+          <p className="footer-link">Contact</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
